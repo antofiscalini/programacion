4289c6b1ce8164e07c8cc5fa1c8c5bad2451878f
Antonella Fiscalini <antofiscalini2@gmail.com>
2026-09-13 19:33:11 -0300

    Add files via upload

---
+++ programacion/ejer35.php
@@ -0,0 +1,34 @@
+<?php
+
+$cadena_texto="Hola profesor Alan.";
+
+$cadena_texto=strtolower($cadena_texto);
+
+echo "$cadena_texto <br><br>";
+
+
+$cadena_texto2="Hola profesor Alan.";
+
+$cadena_texto2=strtoupper($cadena_texto2);
+
+echo "$cadena_texto2 <br><br>";
+
+$cadena_texto3="Hola profesor Alan.";
+
+$cadena_texto3=strtolower($cadena_texto3);
+
+echo "$cadena_texto3 <br><br>";
+
+
+$cadena_texto4="hola profesor Alan.";
+
+$cadena_texto4=ucfirst($cadena_texto4);
+
+echo "$cadena_texto4 <br><br>";
+
+
+$cadena_texto5="Hola profesor Alan.";
+
+$cadena_texto5=ucwords($cadena_texto5);
+
+echo "$cadena_texto5 <br><br>";
